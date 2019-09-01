2e4a7626362d260ae81b4448427b77aaee59df9f
Michael Patel <michael.patel94@gmail.com>
2019-09-01 14:50:54 -0400

    Adding test script that uses sqlalchemy

---
+++ test.py
@@ -0,0 +1,18 @@
+"""
+Michael Patel
+September 2019
+
+File description:
+    Test script to try out ideas with SQL, Pandas, and Python
+
+"""
+################################################################################
+# Imports
+import pandas as pd
+from sqlalchemy import create_engine
+
+################################################################################
+# Main
+if __name__ == "__main__":
+    # create SQL engine
+    engine = create_engine()
